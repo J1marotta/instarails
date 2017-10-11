98129b90aacac4cfa81a591a58d52e26d0072ba7
J1marotta <jim_is55@hotmail.com>
2017-10-11 15:09:26 +1100

    add comments to helpers

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,18 +1,18 @@
 module PhotosHelper
-
-
-#  Function to return true 
+	# Method returns true if you pass all the conditions.
 	def owns_photo?(photo)
 
-
+		# are you signed in?
 		if !user_signed_in?
 			return false
 		end
 
+		# does your Id match the photo creator id
 		if current_user.id != photo.user_id
 			return false
 		end
 
+		# okay return true
 		return true
 	end
 end
